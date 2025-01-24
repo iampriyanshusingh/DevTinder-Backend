9bf6316e1a80dd098a548395e927a661007987a0
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-01-24 15:36:17 +0530

    `Added new API endpoints for retrieving user data by email and all users`

--- src/app.js
+++ src/app.js
@@ -3,11 +3,9 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
-
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-
   const user = new User(req.body);
 
   try {
@@ -15,6 +13,44 @@ app.post("/signup", async (req, res) => {
     res.send("Data Added Successfully");
   } catch (err) {
     res.status(400).send("Failed to connect");
+  }
+});
+
+//getting data from the Email (All Data with same Email Id)
+app.get("/email", async (req, res) => {
+  const emailId = req.body.email;
+  try {
+    const user = await User.find({ email: emailId });
+    res.send(user);
+  } catch (err) {
+    res.status(404).send("Email Not Found");
+  }
+});
+
+//getting all the user's data, basically for the feed API
+app.get("/user", async (req, res) => {
+  const user = await User.find({}); //passing an empty object will give all the user's data
+  try {
+    res.send(user);
+  } catch (err) {
+    res.status(404).send("User not Found");
+  }
+});
+
+//getting one data of the duplicate Email Id
+app.get("/emailOne", async (req, res) => {
+  const emailId = req.body.email;
+  console.log(emailId);
+  try {
+    const user = await User.findOne({ email: emailId });
+    //if we dont pass any object in the findOne, it will give the any arbitary info from the database
+    if (!user) {
+      res.status(404).send("Email Not Found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(404).send("Data Not Found");
   }
 });
 
