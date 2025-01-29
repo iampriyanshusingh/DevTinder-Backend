1ccc91da045979864eb758d2262d84982a078e22
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-01-29 18:30:41 +0530

    `Added validation logic using validator library and exported validation function`

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,3 +1,17 @@
-const validation = () => {
-    
+const validator = require('validator');
+
+const validation = (req) => {
+   const {firstName, lastName, email, password  } =  req.body;
+
+   if(!firstName || !lastName){
+    throw new Error("Name is not Valid");
+   } else if(!validator.isEmail(email)){
+    throw new Error("Email is not Valid");
+   }else if(!validator.isStrongPassword(password)){
+    throw new Error("Please Enter Strong Password");
+   }
 } 
+
+module.exports= {
+    validation,
+}
