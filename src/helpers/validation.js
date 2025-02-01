f0af3d094a6af9d6bc0f0302ef89ab1e53dc2483
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-02-01 21:04:12 +0530

    ` Added validation for profile edit and password update, updated profile routes to include validation and password hashing. `

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,17 +1,46 @@
-const validator = require('validator');
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const validation = (req) => {
-   const {firstName, lastName, email, password  } =  req.body;
+  const { firstName, lastName, email, password } = req.body;
 
-   if(!firstName || !lastName){
+  if (!firstName || !lastName) {
     throw new Error("Name is not Valid");
-   } else if(!validator.isEmail(email)){
+  } else if (!validator.isEmail(email)) {
     throw new Error("Email is not Valid");
-   }else if(!validator.isStrongPassword(password)){
+  } else if (!validator.isStrongPassword(password)) {
     throw new Error("Please Enter Strong Password");
-   }
-} 
+  }
+};
 
-module.exports= {
-    validation,
-}
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "email",
+    "Gender",
+    "age",
+    "skills",
+  ];
+
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
+
+  return isEditAllowed;
+};
+
+const validatePasswordByUser = async (req) => {
+  const oldPassword = req.body;
+  const currentPassword = req.user.password;
+
+  const checkPassword = await bcrypt.compare(oldPassword, currentPassword);
+
+  return checkPassword;
+};
+
+module.exports = {
+  validation,
+  validateEditProfileData,
+  validatePasswordByUser,
+};
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,16 +1,62 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { authUser } = require("../middlewares/auth");
+const bcrypt = require("bcrypt");
+const {
+  validateEditProfileData,
+  validatePasswordByUser,
+} = require("../helpers/validation");
 
 //getting data
-profileRouter.get("/profile", authUser, async (req, res) => {
-    try {
-      const user = req.user;
-      res.send(user);
-    } catch (err) {
-      res.status(400).send("Something Went Wrong");
+profileRouter.get("/profile/view", authUser, async (req, res) => {
+  try {
+    const user = req.user;
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+//updating the data
+profileRouter.patch("/profile/edit", authUser, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
     }
-  });
-  
- 
+
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+    await loggedInUser.save();
+
+    res.json({
+      message: `${loggedInUser.firstName}, Your Profile is updated Successfully`,
+      data: loggedInUser,
+    });
+  } catch (err) {
+    res.statusMessage(400).send("ERROR : " + err.message);
+  }
+});
+
+//updating the password
+profileRouter.patch("/profile/password", authUser, async (req, res) => {
+  try {
+    // const isPasswordValid = await validatePasswordByUser(req);
+    // if (!isPasswordValid) {
+    //   res.status(401).send("Invalid Old Password");
+    // }
+    const loggedInUser = req.user;
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    loggedInUser.password = hashedPassword;
+    await loggedInUser.save();
+    res.json({
+      message: `${loggedInUser.firstName}, Your Password is updated Successfully`,
+      data: loggedInUser,
+    });
+  } catch (err) {
+    res.status(400).send("Invalid Request : " + err.message);
+  }
+});
+
 module.exports = profileRouter;
