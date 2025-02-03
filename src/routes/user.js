08f9c8a8014a49539c07704cd432a9693982ab9c
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-02-04 02:20:43 +0530

    ` done making connections API `

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,8 @@ const userRouter = express.Router();
 const { authUser } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
+const USER_SAFE_DATA = "firstName lastName age Gender skills";
+
 //getting all the pending connection request for the loggedIn user
 userRouter.get("/user/requests/received", authUser, async (req, res) => {
   try {
@@ -12,7 +14,8 @@ userRouter.get("/user/requests/received", authUser, async (req, res) => {
       status: "interested",
     }).populate(
       "fromUserId",
-      ["firstName", "lastName"]
+      USER_SAFE_DATA
+      // ["firstName", "lastName"]
       //if i dont pass the array so it will return the whole data of that User
       // i can also pass the strings instead of the array like "firstName lastName"
     );
@@ -20,6 +23,32 @@ userRouter.get("/user/requests/received", authUser, async (req, res) => {
     res.json({ message: "data sent successfully", data: connectionRequests });
   } catch (err) {
     res.status(400).json({ message: "Something Went Wrong" + err.message });
+  }
+});
+
+userRouter.get("/user/connections", authUser, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = connectionRequest.map((row) => {
+      if (row.fromUserId._id.toString() === row.toUserId._id.toString()) {
+        return row.toUserId;
+      }
+      row.fromUserId;
+    });
+
+    res.json({ data });
+  } catch (err) {
+    res.status(400).send({ message: "Something Went Wrong" + err.message });
   }
 });
 
