141734d49e06490180dd30464e50f8ea268de4f6
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-02-03 18:19:00 +0530

    `Refactor request endpoint to accept status and toUserId as parameters`

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,16 +1,62 @@
 const express = require("express");
 const requestRouter = express.Router();
 const { authUser } = require("../middlewares/auth");
+const User = require("../models/user");
 
-requestRouter.post("/sendConnectionRequest", authUser, async (req, res) => {
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  authUser,
+  async (req, res) => {
     try {
-      console.log("Sending a connection request");
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      //handling the case of diff allowed Status
+      const allowedStatus = ["interested", "ignored"];
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid status type" + status });
+      }
+
+      //handling the case, where userId doesn't exists
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      //handling the case, if request is already being sent or in pending status
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+
+      if (existingConnectionRequest) {
+        return res
+          .status(400)
+          .send({ message: "Connection Request Already Exists" });
+      }
+
+      //making new instance
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message: "Connection Request Sent Successfully",
+        data,
+      });
     } catch (err) {
       res.status(400).send("Something Went Wrong : " + err.message);
     }
-    const user = req.user;
-    res.send(user.firstName + " sent the Connection Request");
-  });
-
+  }
+);
 
 module.exports = requestRouter;
