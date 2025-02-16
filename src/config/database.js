b746734a3a7f34b119166da88493176e2f92b0cd
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-02-16 22:32:41 +0530

    ` created .env file `

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const mongoose = require("mongoose");
 
 const connectDB =  async () =>{
-    await mongoose.connect("mongodb+srv://iampriyanshusingh:5w7wNHiEuxL0YjTY@iampriyanshusingh.gdzxn.mongodb.net/devTinder");
+    await mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = connectDB;
